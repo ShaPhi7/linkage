7355c4ab094f63680a149b789fa069656f7294fb
Shaun Phillips <61982125+ShaPhi7@users.noreply.github.com>
2020-06-18 19:55:52 +0100

    Create `board` database table

--- dbmodel.sql
+++ dbmodel.sql
@@ -20,14 +20,13 @@
 
 -- Example 1: create a standard "card" table to be used with the "Deck" tools (see example game "hearts"):
 
--- CREATE TABLE IF NOT EXISTS `card` (
---   `card_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
---   `card_type` varchar(16) NOT NULL,
---   `card_type_arg` int(11) NOT NULL,
---   `card_location` varchar(16) NOT NULL,
---   `card_location_arg` int(11) NOT NULL,
---   PRIMARY KEY (`card_id`)
--- ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+CREATE TABLE IF NOT EXISTS `board` (
+  `board_x` smallint(5) unsigned NOT NULL,
+  `board_y` smallint(5) unsigned NOT NULL,
+  `piece_color` char(10) DEFAULT NULL,
+  `head` smallint(5) unsigned NOT NULL DEFAULT 0,
+  PRIMARY KEY (`board_x`,`board_y`)
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 
 -- Example 2: add a custom field to the standard "player" table
