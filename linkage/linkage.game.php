25de25f5e966752d75ebd906ec7241c92da12c0c
Shaun Phillips <smphillips@alumni.york.ac.uk>
2024-11-13 12:49:04 +0000

    Minor tweaks to update code version

--- linkage/linkage.game.php
+++ linkage/linkage.game.php
@@ -71,7 +71,7 @@ class Linkage extends Table
             $color = array_shift( $default_colors );
             $values[] = "('".$player_id."','$color','".$player['player_canal']."','".addslashes( $player['player_name'] )."','".addslashes( $player['player_avatar'] )."')";
         }
-        $sql .= implode( $values, ',' );
+        $sql .= implode( ',', $values );
         self::DbQuery( $sql );
         self::reloadPlayersBasicInfos();
 
